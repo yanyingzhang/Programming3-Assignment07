5b344fbe10defe2ab0ff0c3cd62fed5227c51999
Angelaaa <yanying129@gmail.com>
2018-08-02 19:27:15 -0400

    Create interface

--- YanyingZhang_Lab07_Sec-02/Form1.Designer.cs
+++ YanyingZhang_Lab07_Sec-02/Form1.Designer.cs
@@ -28,20 +28,311 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.calculateGB = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.checkGB = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBtn = new System.Windows.Forms.Button();
+            this.displayGB = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.calculateGB.SuspendLayout();
+            this.checkGB.SuspendLayout();
+            this.displayGB.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // calculateGB
+            // 
+            this.calculateGB.Controls.Add(this.calculateBtn);
+            this.calculateGB.Controls.Add(this.textBox1);
+            this.calculateGB.Controls.Add(this.label1);
+            this.calculateGB.Location = new System.Drawing.Point(12, 12);
+            this.calculateGB.Name = "calculateGB";
+            this.calculateGB.Size = new System.Drawing.Size(274, 163);
+            this.calculateGB.TabIndex = 0;
+            this.calculateGB.TabStop = false;
+            this.calculateGB.Text = "(1) Calculate Asynchronously";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get Factorial of:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(22, 95);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(98, 40);
+            this.calculateBtn.TabIndex = 2;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            // 
+            // checkGB
+            // 
+            this.checkGB.Controls.Add(this.checkBtn);
+            this.checkGB.Controls.Add(this.textBox2);
+            this.checkGB.Controls.Add(this.label2);
+            this.checkGB.Location = new System.Drawing.Point(12, 190);
+            this.checkGB.Name = "checkGB";
+            this.checkGB.Size = new System.Drawing.Size(274, 201);
+            this.checkGB.TabIndex = 1;
+            this.checkGB.TabStop = false;
+            this.checkGB.Text = "(2) Check for Even/Odd";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Input Number:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.Location = new System.Drawing.Point(47, 95);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(200, 40);
+            this.checkBtn.TabIndex = 2;
+            this.checkBtn.Text = "Check For Even/Odd";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            // 
+            // displayGB
+            // 
+            this.displayGB.Controls.Add(this.label6);
+            this.displayGB.Controls.Add(this.listView1);
+            this.displayGB.Controls.Add(this.textBox6);
+            this.displayGB.Controls.Add(this.textBox5);
+            this.displayGB.Controls.Add(this.textBox4);
+            this.displayGB.Controls.Add(this.textBox3);
+            this.displayGB.Controls.Add(this.label5);
+            this.displayGB.Controls.Add(this.label4);
+            this.displayGB.Controls.Add(this.label3);
+            this.displayGB.Controls.Add(this.button3);
+            this.displayGB.Controls.Add(this.button2);
+            this.displayGB.Controls.Add(this.button1);
+            this.displayGB.Controls.Add(this.radioButton3);
+            this.displayGB.Controls.Add(this.radioButton2);
+            this.displayGB.Controls.Add(this.radioButton1);
+            this.displayGB.Location = new System.Drawing.Point(302, 12);
+            this.displayGB.Name = "displayGB";
+            this.displayGB.Size = new System.Drawing.Size(481, 379);
+            this.displayGB.TabIndex = 2;
+            this.displayGB.TabStop = false;
+            this.displayGB.Text = "(3) Display List of Values and Search";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(19, 48);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Integers";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(118, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(93, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Doubles";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(217, 48);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(76, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Chars";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate Values";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(376, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(376, 189);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Display";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Input Value for Search:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(147, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Input Low Index:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Input high index:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(270, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(270, 156);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(270, 203);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(118, 327);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(341, 26);
+            this.textBox6.TabIndex = 12;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(19, 98);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(75, 255);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(133, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(286, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Output of values between low and high:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(812, 420);
+            this.Controls.Add(this.displayGB);
+            this.Controls.Add(this.checkGB);
+            this.Controls.Add(this.calculateGB);
             this.Name = "Form1";
             this.Text = "Asynchronously Programming";
+            this.calculateGB.ResumeLayout(false);
+            this.calculateGB.PerformLayout();
+            this.checkGB.ResumeLayout(false);
+            this.checkGB.PerformLayout();
+            this.displayGB.ResumeLayout(false);
+            this.displayGB.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox calculateGB;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox checkGB;
+        private System.Windows.Forms.Button checkBtn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox displayGB;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label6;
     }
 }
 
